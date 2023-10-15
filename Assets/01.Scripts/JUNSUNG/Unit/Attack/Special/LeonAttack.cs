6c60133a6e11156afb7d01112bcf35c09a846966
JUNSUNG06 <forest2847@gmail.com>
2023-10-16 00:19:21 +0900

    leon

---
+++ Assets/01.Scripts/JUNSUNG/Unit/Attack/Special/LeonAttack.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeonAttack : UnitAttackType
+{
+    [Space]
+    [SerializeField]
+    private float buffTime = 10f;
+    [SerializeField]
+    private int buffCnt = 0;
+    [SerializeField]
+    private int buffAmount = 110;
+
+    public override void Attack()
+    {
+        base.Attack();
+
+        float damage = isCritical ? this.damage * 2 : this.damage;
+
+        if (controller.Target.TryGetComponent<IDamageable>(out IDamageable t))
+        {
+            t.GetDamaged(damage, out bool isKill);
+
+            if (isKill)
+            {
+                controller.Target = null;
+                controller.Movement.Stop();
+
+                if (buffCnt < 10)
+                    StartCoroutine(Buff());
+            }
+        }
+
+        isCritical = false;
+    }
+
+    private IEnumerator Buff()
+    {
+        foreach(StatusType t in Enum.GetValues(typeof(StatusType)))
+        {
+            controller.Stat.Buff(t, buffAmount, buffTime);
+        }
+        buffCnt++;
+
+        yield return new WaitForSeconds(buffTime);
+
+        buffCnt--;
+    }
+}
